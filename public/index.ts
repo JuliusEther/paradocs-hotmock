de2db56fd1d2cd9d29c2108f797bf95f75a3bbaa
JuliusEther <julius.ether@gmail.com>
2022-02-20 06:59:42 +0900

    基本的な検索を実装

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,75 @@
+import { Paradocs } from "./paradocs.js"
 
-window.onload = function() {
-    console.log("Hello")
+class InternalState {
+    static paradocs: Paradocs.State = null;
 }
 
+window.onload = function () {
+    console.log("Hello")
+
+    document.getElementById("fetch").onclick = () => {
+        data_fetch()
+    }
+
+    document.getElementById("target").onchange = () => {
+        change_search_box()
+    }
+}
+
+function data_fetch() {
+    InternalState.paradocs = new Paradocs.State()
+    InternalState.paradocs.set_data_source((<HTMLInputElement>document.getElementById("data_source")).value)
+    InternalState.paradocs.fetch_data(() => {
+        console.log("fetch completed.")
+    })
+}
+
+function change_search_box() {
+    clear_list()
+    insert_title()
+
+    const word = (<HTMLInputElement>document.getElementById("target")).value
+
+    console.log("Title fullmatch")
+    for (const item of InternalState.paradocs.filter_records(word, true, true)) {
+        add_list(item)
+    }
+
+    console.log("Title any")
+    for (const item of InternalState.paradocs.filter_records(word, false, true)) {
+        add_list(item)
+    }
+
+    console.log("Tag fullmatch")
+    for (const item of InternalState.paradocs.filter_records(word, true, false, true)) {
+        add_list(item)
+    }
+
+    console.log("Tag any")
+    for (const item of InternalState.paradocs.filter_records(word, false, false, true)) {
+        add_list(item)
+    }
+
+}
+
+function clear_list() {
+    const table = (<HTMLTableElement>document.getElementById("result"))
+    table.innerHTML = ""
+}
+
+function insert_title(){
+    const table = (<HTMLTableElement>document.getElementById("result"))
+    const header = table.insertRow(-1)
+    header.insertCell(0).textContent = "Title"
+    header.insertCell(1).textContent = "Tag"
+    header.insertCell(2).textContent = "Description"
+}
+
+function add_list(records: Paradocs.Record) {
+    const target = (<HTMLTableElement>document.getElementById("result")).tBodies
+    const new_row = target[0].insertRow(-1)
+    new_row.insertCell(0).textContent = records.Title
+    new_row.insertCell(1).textContent = records.Tag.join(", ")
+    new_row.insertCell(2).textContent = records.Description
+
+}
---
+++ public/paradocs.ts
@@ -0,0 +1,97 @@
+export namespace Paradocs {
+    export type Record = {
+        ID: string
+        Title: string
+        Tag: Array<string>
+        Description: string
+    }
+
+    export class State {
+
+        private data: Array<Record>
+        private data_source: string | null = null
+
+        public async fetch_data(on_conplete: Function) {
+            if (this.data_source != null) {
+                const result = await (await fetch(this.data_source, { method: "GET" })).json()
+                this.data = this.parse_record(result)
+                on_conplete()
+            }
+        }
+
+        private parse_record(data: any): Array<Record> {
+            const result: Array<Record> = []
+
+            if (!(data instanceof Array)) {
+                throw TypeError("not array")
+            }
+
+            for (const item of data) {
+                const record = this.get_record(item)
+                if (record === null) {
+                    break;
+                }
+                result.push(record)
+            }
+
+            return result;
+        }
+
+        private get_record(record: Record): Record | null {
+            if (record !== null &&
+                typeof record === "object" &&
+                typeof record.ID === "string" &&
+                typeof record.Title === "string" &&
+                record.Tag instanceof Array &&
+                typeof record.Description === "string"
+            ) {
+                return record
+            } else {
+                return null
+            }
+
+        }
+
+        public set_data_source(data_source: string) {
+            this.data_source = data_source
+        }
+
+        public * filter_records(word: string, full_mactch: boolean = true, match_title: boolean = true, match_tag: boolean = false) {
+            if (match_title) {
+                if (full_mactch) {
+                    for (const item of this.data) {
+                        if (item.Title.match("^" + word + "$")) {
+                            yield item
+                        }
+                    }
+                } else {
+                    for (const item of this.data) {
+                        if (item.Title.indexOf(word) !== -1) {
+                            yield item
+                        }
+                    }
+                }
+            }
+
+            if (match_tag) {
+                if (full_mactch) {
+                    for (const item of this.data) {
+                        for (const tag of item.Tag) {
+                            if (tag.match("^" + word + "$")) {
+                                yield item
+                            }
+                        }
+                    }
+                } else {
+                    for (const item of this.data) {
+                        for (const tag of item.Tag) {
+                            if (tag.indexOf(word) !== -1) {
+                                yield item
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
